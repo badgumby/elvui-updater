ff7e21bf6ac396984d35d69b24172bd5ecba69bc
BADGumby <badgumby@users.noreply.github.com>
2020-09-14 15:14:32 -0500

    Added better version checking. Skips if already current.

--- elvui-updater.py
+++ elvui-updater.py
@@ -8,6 +8,21 @@ import zipfile
 # You will need to set this to the WoW addons directory
 wow_addons = r"F:\Games\Blizzard Games\World of Warcraft\_retail_\Interface\AddOns"
 
+pattern = re.compile("## Version: \d+\.\d+")
+
+# Get currently installed version
+elvFile = wow_addons + "\ElvUI\ElvUI.toc"
+if os.path.exists(elvFile):
+	elvOpen = open(elvFile, 'r')
+	elvRead = elvOpen.read()
+	elvOpen.close()
+	matches = re.findall(pattern, elvRead)
+	myVer = matches[0].replace("## Version: ", "")
+else:
+	myVer = "Not installed"
+
+print("Currently installed version of ElvUI: " + myVer)
+
 myTemp = tempfile.gettempdir()
 
 elv = "\/downloads\/elvui-.*zip"
@@ -39,21 +54,17 @@ for jibb in links:
 i = 0
 if myLink != "Not Found":
 	print("File found at: " + myLink)
-	myVer = re.findall(ver, str(myLink))
-	num = "## Version: " + myVer[0]
-	with open(wow_addons + "\ElvUI\ElvUI.toc") as search:
-		for line in search:
-			line = line.rstrip()
-			if num == line:
-				print("ElvUI is already the newest version: " + myVer[0])
-				i = 1
+	webVer = re.findall(ver, str(myLink))
+	if webVer[0] == myVer:
+		print("ElvUI is already the newest version: " + myVer)
+		print("Skipping download and install. Please check back later for a newer version.")
+		i = 1
 	if i == 0:
 		print("ElvUI is not the newest version available.")
 		print("Downloading to: " + outf + "...")
 		download(myLink, outf)
 		unzipme(outf, wow_addons)
-		print("ElvUI has been updated to version " + str(myVer[0]) + ".")
+		print("ElvUI has been updated to version " + str(webVer[0]) + ".")
 else:
 	print("Download link was not found!")
 	print("Could not update ElvUI")
-
