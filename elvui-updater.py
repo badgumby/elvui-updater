a2087785f08d57c7ad2c3cc937b739a3f0fb5d80
BADGumby <badgumby@users.noreply.github.com>
2020-09-14 09:46:52 -0500

    Removed requirement for urlopen. Added current installed version check.

--- elvui-updater.py
+++ elvui-updater.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen, urlretrieve
 import re
 import requests
 import tempfile
@@ -24,10 +23,9 @@ def unzipme(myFile, myPath):
 	with zipfile.ZipFile(myFile, 'r') as zip_ref:
 		zip_ref.extractall(myPath)
 
-req = Request("https://www.tukui.org/download.php?ui=elvui")
-html_page = urlopen(req)
+elvPage = requests.get("https://www.tukui.org/download.php?ui=elvui")
 
-soup = BeautifulSoup(html_page, "lxml")
+soup = BeautifulSoup(elvPage.text, "lxml")
 
 links = []
 for link in soup.findAll('a'):
@@ -38,14 +36,23 @@ for jibb in links:
 	if re.search(elv, str(jibb)):
 		myLink = base + str(jibb)
 		
-
+i = 0
 if myLink != "Not Found":
 	print("File found at: " + myLink)
-	print("Downloading to: " + outf + "...")
 	myVer = re.findall(ver, str(myLink))
-	download(myLink, outf)
-	unzipme(outf, wow_addons)
-	print("ElvUI has been updated to version " + str(myVer[0]) + ".")
+	num = "## Version: " + myVer[0]
+	with open(wow_addons + "\ElvUI\ElvUI.toc") as search:
+		for line in search:
+			line = line.rstrip()
+			if num == line:
+				print("ElvUI is already the newest version: " + myVer[0])
+				i = 1
+	if i == 0:
+		print("ElvUI is not the newest version available.")
+		print("Downloading to: " + outf + "...")
+		download(myLink, outf)
+		unzipme(outf, wow_addons)
+		print("ElvUI has been updated to version " + str(myVer[0]) + ".")
 else:
 	print("Download link was not found!")
 	print("Could not update ElvUI")
