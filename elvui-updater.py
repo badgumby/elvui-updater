7b5600055c54e5ca653bb37efe7374dff67dd895
BADGumby <badgumby@users.noreply.github.com>
2020-11-03 12:11:36 -0600

    update elvui-updater
    Add color to output

--- elvui-updater.py
+++ elvui-updater.py
@@ -14,16 +14,26 @@ import zipfile # Used to uncompress zip file
 import time # Used to sleep
 from tqdm import tqdm # Used for download progress bar
 
+# Colors for highlighting text (not all are used)
+class bcolors:
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+
 # Reads the config.txt file next to the script to get the 'Addons' directory
 if os.path.exists('config.txt'):
 	cfg = open('config.txt','r')
 	cfgtxt = cfg.readline()
 	cfg.close()
 	if cfgtxt == "directory=" or cfgtxt == "":
-		print("WoW Addon directory not selected.")
+		print(f"{bcolors.BOLD}{bcolors.BLUE}WoW Addon directory not selected.{bcolors.ENDC}")
 		newpath = askdirectory(title='Select the "Addons" directory',initialdir = '/', mustexist = 'TRUE')
 		if newpath == "":
-			print("No directory selected. Exiting...")
+			print(f"{bcolors.BOLD}{bcolors.RED}No directory selected. Exiting...{bcolors.ENDC}")
 			exit()
 		else:
 			cfg = open('config.txt','w+')
@@ -33,10 +43,10 @@ if os.path.exists('config.txt'):
 	else:
 		wow_addons = cfgtxt.replace("directory=","")
 else:
-	print("Updater config file not found.")
+	print(f"{bcolors.BOLD}{bcolors.RED}Updater config file not found.{bcolors.ENDC}")
 	newpath = askdirectory(title='Select the "Addons" directory',initialdir = '/', mustexist = 'TRUE')
 	if newpath == "":
-		print("No directory selected. Exiting...")
+		print(f"{bcolors.BOLD}{bcolors.RED}No directory selected. Exiting...{bcolors.ENDC}")
 		exit()
 	else:
 		cfg = open('config.txt','w+')
@@ -59,7 +69,7 @@ if os.path.exists(elvFile):
 else:
 	myVer = "Not installed"
 
-print("Currently installed version of ElvUI: " + myVer)
+print(f"Currently installed version of ElvUI: {bcolors.BOLD}{bcolors.BLUE}" + myVer + f"{bcolors.ENDC}")
 
 myTemp = tempfile.gettempdir()
 
@@ -70,19 +80,20 @@ outf = os.path.join(myTemp, "elvui.zip")
 
 # Download file function
 def download(url, file_name):
-    resp = requests.get(url, stream=True)
-    total = int(resp.headers.get('content-length', 0))
-    
-    with open(file_name, 'wb') as file, tqdm(
-    	desc=file_name,
-    	total=total,
-    	unit='iB',
-    	unit_scale=True,
-    	unit_divisor=1024,
+	resp = requests.get(url, stream=True)
+	total = int(resp.headers.get('content-length', 0))
+	print(f'{bcolors.BOLD}{bcolors.CYAN}')
+	with open(file_name, 'wb') as file, tqdm(
+		desc=file_name,
+		total=total,
+		unit='iB',
+		unit_scale=True,
+		unit_divisor=1024,
     ) as bar:
-        for data in resp.iter_content(chunk_size=1024):
-            size = file.write(data)
-            bar.update(size)
+		for data in resp.iter_content(chunk_size=1024):
+			size = file.write(data)
+			bar.update(size)
+	print(f'{bcolors.ENDC}')
 
 # Unzip file function
 def unzipme(myFile, myPath):
@@ -107,18 +118,18 @@ if myLink != "Not Found":
 	print("File found at: " + myLink)
 	webVer = re.findall(ver, str(myLink))
 	if webVer[0] == myVer:
-		print("ElvUI is already the newest version: " + myVer)
+		print(f"ElvUI is already the newest version: {bcolors.BOLD}{bcolors.GREEN}" + myVer + f"{bcolors.ENDC}")
 		print("Skipping download and install. Please check back later for a newer version.")
 		i = 1
 	if i == 0:
-		print("ElvUI is not the newest version available.")
+		print(f"{bcolors.BOLD}{bcolors.YELLOW}ElvUI is not the newest version available.{bcolors.ENDC}")
 		print("Downloading to: " + outf + "...")
 		download(myLink, outf)
 		print("Extracting to " + wow_addons)
 		unzipme(outf, wow_addons)
-		print("ElvUI has been updated to version " + str(webVer[0]) + ".")
+		print(f"ElvUI has been updated to version {bcolors.BOLD}{bcolors.GREEN}" + str(webVer[0]) + f"{bcolors.ENDC}.")
 else:
-	print("Download link was not found!")
-	print("Could not update ElvUI")
+	print(f"{bcolors.BOLD}{bcolors.RED}Download link was not found!{bcolors.ENDC}")
+	print(f"{bcolors.BOLD}{bcolors.RED}Could not update ElvUI{bcolors.ENDC}")
 
 time.sleep(5)
